c1a289db516459b5f2903cdd5e672c8d69126087
Kaviya Panneerselvam <kaviya0925@gmail.com>
2021-08-17 19:30:20 -0500

    Anugular Studio 2

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-
+equipment :string[]=[
+  'Habitat dome','Drones','Food container','Oxygen tanks'
+]
+equipmentBeingEdited :string =null;
   constructor() { }
 
   ngOnInit() {
+  }
+  addEquipment(name:string){
+    this.equipment.push(name);
+  }
+  removeEquipment(name:string){
+    let index= this.equipment.indexOf(name);
+    this.equipment.splice(index,1);
+  }
+  edit(device){
+   this.equipmentBeingEdited=device;
+  }
+  save(name:string , equip:string){
+    let index=this.equipment.indexOf(equip);
+    this.equipment.splice(index,1,name);
+    this.equipmentBeingEdited=null;
   }
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
-
+   experiment :string[]=[
+    'Mars soil sample','Plant growth in habitat','Human bone density'
+   ]
+   workBeingEdited :string=null;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+  remove(experi:string){
+    let index=this.experiment.indexOf(experi);
+    this.experiment.splice(index,1);
+  }
+  add(newExperi){
+    this.experiment.push(newExperi);
+  }
+  edit(editWork){
+    this.workBeingEdited=editWork;
+  }
+  save(update,experi){
+    let index=this.experiment.indexOf(experi)
+    this.experiment.splice(index,1,update);
+    this.workBeingEdited=null;
   }
 
 }
